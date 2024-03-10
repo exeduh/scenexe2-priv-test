d57e27441a57265fe653a919e896f5897ed22708
exeduh <159093212+exeduh@users.noreply.github.com>
2024-03-10 19:36:52 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 const fetch = require('fetch').fetchUrl
-
+const path = require('path')
 const main = function(scenexe2) {
   let options = {
     parentPort: {
@@ -17,11 +17,20 @@ const main = function(scenexe2) {
   let data = scenexe2.run(options)
 }
 
-fetch('https://beta-scenexe2.glitch.me/scenexe2.js', function(a, b, c) {
+const localFilePath = path.join(__dirname, 'scenexe2.js');
+​
+fs.readFile(localFilePath, 'utf8', (err, fileContent) => {
+  if (err) {
+    console.error('Error reading local file:', err);
+    return;
+  }
+​
   let __module__ = {
     exports: {}
-  }
-  let s = c.toString().replace(`module`, `__module__`)
-  eval(s)
-  main(__module__.exports)
-})
+  };
+​
+  let s = fileContent.toString().replace(`module`, `__module__`);
+  eval(s);
+​
+  main(__module__.exports);
+});
