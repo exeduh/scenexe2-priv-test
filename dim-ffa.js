ac157448dec117c7c57c04ed520a834e4bec393c
exeduh <159093212+exeduh@users.noreply.github.com>
2024-03-10 18:56:24 +0800

    Update d-2teams.js

--- dim-ffa.js
+++ dim-ffa.js
@@ -1,15 +1,14 @@
 !function() {
  const dim = dimension.create({
-    mapSize: 2000,
-    name: 'test',
-    type: '4teams',
+    mapSize: 6000,
+    name: '2teams',
+    type: '2teams',
     freeJoin: true,
-    allowScale: true,
-    removeFallens: true,
-    displayName: '4 Teams',
-    displayRadiant: 0.3,
-    displayColor: -6,
-    walls: [],
+    allowScale: false,
+    removeFallens: false,
+    displayName: '2 Teams',
+    displayColor: 4,
+    walls: [[-5200, 0, 800, 6000, 1], [5200, 0, 800, 6000, 2]],
     gates: [],
     background: {
       r: 205,
@@ -22,66 +21,154 @@
       b: 200
     },
     gridSize: 25,
-    maxPolygonSides: 8,
+    maxPolygonSides: 13,
     maxPolygonCount: 30,
     spawnPlayer: function(team, tank) {
-      tank.invincible = false
-      tank.invincibleTime = 0
+      tank.invincible = true;
+      tank.update();
+
+      const yValues = [0, -2500, 2500, 5000, 5000];
+      const randomY = yValues[Math.floor(Math.random() * yValues.length)];
+
+      let xValue;
+      if (team === 2) {
+        xValue = 5200;
+      } else if (team === 1) {
+        xValue = -5200;
+      } else {
+        xValue = dim.mapSize - 2000;
+      }
+
+      tank.x = xValue;
+      tank.y = randomY;
+
       setTimeout(function() {
-        if(tank.ws.sendPacket) {
-          tank.ws.sendPacket('announcement', 'Welcome to a scenexe2 private server! Type /a to get command access')
-        }
-      })
-      return [0, 0]
+      }, 0);
+
+      return [tank.x, tank.y];
     }
- })
-  
- generator.wormhole({
-    x: 500,
-    y: 0,
-    size: 75,
-    type: 2,
-    dim: dim,
-    action: function(tank) {
-      tank.radiant ++
-      dimension.sendTankTo({
-        tank: tank,
-        dim: 'test',
-      })
-    }
- })
-  
+ });
+
  setTimeout(function() {
-    
-    generator.polygon({
-      x: 500,
-      y: 0,
-      d: 2 * Math.PI * Math.random(),
-      sides: -1,
+    const dr1 = generator.tank({
       dim: dim,
-      radiant: 4
-    })
-    
-    const bot = generator.tank({
-      dim: dim,
-      x: -1000,
+      x: 5200,
       y: 0,
-      name: 'Test Bot',
-      weapon: 'quad',
-      body: 'repeller',
-      score: 0,
-      radiant: 3,
+      name: 'Red Defender',
+      weapon: 'defender',
+      body: 'defender',
+      ai: 'defender',
+      score: 1.8e19,
       static: true,
-      team: 8
-    })
-    bot.firing = true
-    let passive = false
-    setInterval(function() {
-      bot.passive = passive = !passive
-    }, 1000)
-    setInterval(function() {
-      bot.d += 0.1
-    }, 100)
-    
- }, 1000)
+      team: 2
+    });
+    const dr2 = generator.tank({
+      dim: dim,
+      x: 5200,
+      y: 2500,
+      name: 'Red Defender',
+      weapon: 'defender',
+      body: 'defender',
+      ai: 'defender',
+      score: 1.8e19,
+      static: true,
+      team: 2
+    });
+    const dr3 = generator.tank({
+      dim: dim,
+      x: 5200,
+      y: -2500,
+      name: 'Red Defender',
+      weapon: 'defender',
+      body: 'defender',
+      ai: 'defender',
+      score: 1.8e19,
+      static: true,
+      team: 2
+    });
+    const dr4 = generator.tank({
+      dim: dim,
+      x: 5200,
+      y: 5000,
+      name: 'Red Defender',
+      weapon: 'defender',
+      body: 'defender',
+      ai: 'defender',
+      score: 1.8e19,
+      static: true,
+      team: 2
+    });
+    const dr5 = generator.tank({
+      dim: dim,
+      x: 5200,
+      y: -5000,
+      name: 'Red Defender',
+      weapon: 'defender',
+      body: 'defender',
+      ai: 'defender',
+      score: 1.8e19,
+      static: true,
+      team: 2
+    });
+    const db1 = generator.tank({
+      dim: dim,
+      x: -5200,
+      y: 0,
+      name: 'Blue Defender',
+      weapon: 'defender',
+      body: 'defender',
+      ai: 'defender',
+      score: 1.8e19,
+      static: true,
+      team: 1
+    });
+    const db2 = generator.tank({
+      dim: dim,
+      x: -5200,
+      y: 2500,
+      name: 'Blue Defender',
+      weapon: 'defender',
+      body: 'defender',
+      ai: 'defender',
+      score: 1.8e19,
+      static: true,
+      team: 1
+    });
+    const db3 = generator.tank({
+      dim: dim,
+      x: -5200,
+      y: -2500,
+      name: 'Blue Defender',
+      weapon: 'defender',
+      body: 'defender',
+      ai: 'defender',
+      score: 1.8e19,
+      static: true,
+      team: 1
+    });
+    const db4 = generator.tank({
+      dim: dim,
+      x: -5200,
+      y: 5000,
+      name: 'Blue Defender',
+      weapon: 'defender',
+      body: 'defender',
+      ai: 'defender',
+      score: 1.8e19,
+      static: true,
+      team: 1
+    });
+    const db5 = generator.tank({
+      dim: dim,
+      x: -5200,
+      y: -5000,
+      name: 'Blue Defender',
+      weapon: 'defender',
+      body: 'defender',
+      ai: 'defender',
+      score: 1.8e19,
+      static: true,
+      team: 1
+    });
+ }, 1000);
 }()
